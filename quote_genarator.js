16924e3281f9ceba0c15d5f433d491bb8a820499
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-31 19:45:40 +0200

    create quotes using objects

--- quote_genarator.js
+++ quote_genarator.js
@@ -1,7 +1,46 @@
 let p = document.createElement('p');
 let button = document.createElement('button');
 let container = document.getElementById('quote_container');
+let newQuote;
 container.appendChild(p);
 container.appendChild(button);
 button.innerHTML = 'Print Quote';
 p.innerHTML = 'test';
+
+const Quotes = {
+    init: function (fragmentList1, fragmentList2, fragmentList3, fragmentList4){
+        this.fragmentList1 = fragmentList1;
+        this.fragmentList2 = fragmentList2;
+        this.fragmentList3 = fragmentList3;
+        this.fragmentList4 = fragmentList4;
+    },
+    describe: function(){
+        const fragment1 = Math.floor(Math.random() * (this.fragmentList1.length));
+        const fragment2 = Math.floor(Math.random() * (this.fragmentList2.length));
+        const fragment3 = Math.floor(Math.random() * (this.fragmentList3.length));
+        const fragment4 = Math.floor(Math.random() * (this.fragmentList4.length));
+        newQuote = (this.fragmentList1[fragment1] + " " + this.fragmentList2[fragment2] + " " + this.fragmentList3[fragment3] + this.fragmentList4[fragment4]);
+        return newQuote;
+    }
+};
+
+const life = Object.create(Quotes);
+life.init([
+        "Being happy",
+        "Living alone",
+        "Never doing"
+    ],
+    [
+        "is something we do",
+        "is what we need",
+        "is when we shouldn't do"
+    ],
+    [
+        "even though we know we shouldn't",
+        "although we think we don't",
+        "just incase its not for us"
+    ],
+    [
+        "!",
+        "."
+    ]);
