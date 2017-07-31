296c8090f68721ebedce27cc667fc7333db95975
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-31 19:40:25 +0200

    button and p created using js

---
+++ quote_genarator.js
@@ -0,0 +1,7 @@
+let p = document.createElement('p');
+let button = document.createElement('button');
+let container = document.getElementById('quote_container');
+container.appendChild(p);
+container.appendChild(button);
+button.innerHTML = 'Print Quote';
+p.innerHTML = 'test';
