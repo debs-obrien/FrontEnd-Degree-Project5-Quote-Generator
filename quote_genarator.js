e3a87639f84e86477e1b5daa3c0150b9122bafba
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-07-31 22:13:56 +0200

    printing different types of quotes

--- quote_genarator.js
+++ quote_genarator.js
@@ -1,28 +1,8 @@
 const p = document.createElement('p');
 const button = document.createElement('button');
 const container = document.getElementById('quote_container');
+const form = document.createElement('form');
 const amountInput = document.createElement('input');
-const select1 = document.createElement('input');
-const select2 = document.createElement('input');
-const label1 = document.createElement('label');
-const label2 = document.createElement('label');
-const select = [select1, select2];
-select.forEach(function(element){
-    element.setAttribute('type', 'radio');
-    element.setAttribute('name', 'type');
-    container.appendChild(element);
-
-});
-select1.setAttribute('value', 'life');
-select1.setAttribute('value', 'work');
-label1.setAttribute('for', 'type');
-label2.setAttribute('for', 'type');
-label1.innerHTML = 'life';
-label2.innerHTML = 'work';
-
-//better to create a function for the radios and then call it.
-//need to append the label to the radios too
-//then need to make that part work
 
 amountInput.setAttribute('type', 'text');
 amountInput.setAttribute('value', '');
@@ -30,31 +10,73 @@ amountInput.setAttribute('placeholder', 'type a number');
 
 
 container.appendChild(p);
-container.appendChild(amountInput);
-container.appendChild(button);
+container.appendChild(form);
+form.appendChild(amountInput);
+form.appendChild(button);
 
 button.innerHTML = 'Print Quote';
 
 p.innerHTML = 'Click the button to print a quote';
 
-function selectAmount(){
+function createRadios(radio, name, value, label, text){
+    radio = document.createElement('input');
+    radio.setAttribute('type', 'radio');
+    radio.setAttribute('name', name);
+    radio.setAttribute('class', name);
+    radio.setAttribute('id', value);
+    radio.setAttribute('value', value);
+    label = document.createElement('label');
+    label.setAttribute('for', value);
+    label.innerHTML = text;
+    form.appendChild(label);
+    form.appendChild(radio);
+}
+
+createRadios('select1', 'type', 'life', 'label1', 'Life');
+createRadios('select2', 'type', 'work','label2', 'Work');
+
+function getRadioCheckedValue() {
+    let radios = document.getElementsByName('type');
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            console.log(radios[i].value);
+            return radios[i].value;
+        }
+    }
+}
+
+function selectAmount(quote){
     amountInput.setAttribute('value', amountInput.value);
     let amount = amountInput.value;
-    if(amount > 5 || amount < 1){
-        p.innerHTML= 'Sorry you can only choose a maximum of 5 quotes'
+    if(!quote){
+        p.innerHTML= 'Please choose the type of quote';
+    }
+    else if(!amount){
+        p.innerHTML= 'Please choose an amount';
+    }
+    else if(amount > 5 || amount <= 0){
+        p.innerHTML= 'Sorry you can only between 1 and 5 quotes';
     }else{
         p.innerHTML ='';
         for(let i=0; i<amountInput.value; i+=1){
-            p.innerHTML += life.describe() + "<br>";
+            showQuotes(quote);
         }
         button.innerHTML = 'Print more Quotes';
         amountInput.value = '';
     }
 
 }
+function showQuotes(quote){
+    if(quote==='life'){
+        p.innerHTML += life.describe() + "<br>";
+    }else{
+        p.innerHTML += work.describe() + "<br>";
+    }
+}
 
-button.addEventListener('click', function(e){
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    selectAmount();
+    let quote = getRadioCheckedValue();
+    selectAmount(quote);
 });
-
